8e24a6c2de595339da497615108281d24d679411
Chiamei012 <55222671+Chiamei012@users.noreply.github.com>
2020-01-02 23:11:48 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Lottie from 'react-lottie';
+import * as animationData from './data.json'
+
+class App extends React.Component {
+  state = {
+    isStopped: false,
+    isPaused: false
+  };
+  defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    renderer: 'canvas'
+  };
+
+  play = () => this.setState({isStopped: false, isPaused: false});
+  stop = () => this.setState({isStopped: true});
+  pause = () => this.setState({isPaused: !this.state.isPaused});
+
+  render() {
+    const buttonStyle = {
+      display: 'block',
+      margin: '10px auto'
+    };
+
+  return <div>
+    <Lottie
+      options={this.defaultOptions}
+      width={600}
+      isStopped={this.state.isStopped}
+      isPaused={this.state.isPaused}
+    />
+    <button style={buttonStyle} onClick={this.stop}>停止</button>
+    <button style={buttonStyle} onClick={this.play}>播放</button>
+    <button style={buttonStyle} onClick={this.pause}>暂停</button>
+  </div>
+  }
+}
+
+ReactDOM.render(, document.getElementById('root'));
